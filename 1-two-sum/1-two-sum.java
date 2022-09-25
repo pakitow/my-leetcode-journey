e045f6df420246e8ce5e44bc214d6aa58bdf0a06
Francisco Cruzado <90626364+pakitow@users.noreply.github.com>
2022-09-25 18:52:50 -0500

    Time: 100 ms (20.02%), Space: 44.6 MB (77.15%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,16 +1,12 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] output = new int[2];
-        int i=0; 
-        while(i<nums.length){
-            int j = i+1;
-            while(j<nums.length){
+    public int[] twoSum(int[] nums, int target) {        
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
                 if((nums[i]+nums[j])==target){
-                    output[0] = i;
-                    output[1] = j;
-                }j++;                    
-            }i++;
+                    return new int[] {i, j};
+                }
+            }
         }
-        return output;    
+        throw new IllegalArgumentException("uh-oh");
     }
 }
