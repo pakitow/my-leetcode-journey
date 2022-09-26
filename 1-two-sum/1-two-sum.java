eab9762440c96b4040c66ad52917bc78f51d5a71
Francisco Cruzado <90626364+pakitow@users.noreply.github.com>
2022-09-25 19:12:16 -0500

    Time: 8 ms (57.33%), Space: 45.6 MB (40.38%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         Map<Integer, Integer> nummap = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             int complement = target - nums[i];
-            if(nummap.containsKey(complement)){
+            if(nummap.containsKey(target-nums[i])){
                 return new int[] {nummap.get(complement),i};
             }
             nummap.put(nums[i],i);
