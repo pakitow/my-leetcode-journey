ba14005f8f546236d24609e3ca0afb61863ad6c4
Francisco Cruzado <90626364+pakitow@users.noreply.github.com>
2022-10-18 22:32:46 -0500

    Time: 480 ms (24.26%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select name, population, area
 from World
-where population>=(25*power(10,6)) or area>=(3*power(10,6));
+where population>=25000000 or area>=3000000;
